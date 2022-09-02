6f03102e42c452ebdbbbb144c4efc737d1b9dc42
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-01 22:14:58 -0500

    2022-09-01

---
+++ src/main/kotlin/countGoodNodesInBinaryTree.kt
@@ -0,0 +1,18 @@
+// 2022-09-01
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+fun goodNodes(root: TreeNode?): Int {
+    return goodNodes_rec(Integer.MIN_VALUE, root)
+}
+
+fun goodNodes_rec(maxOnThePath: Int, root: TreeNode?): Int {
+    if (root == null) {
+        return 0
+    }
+
+    return if (root.`val` < maxOnThePath) {
+        goodNodes_rec(maxOnThePath, root.left) + goodNodes_rec(maxOnThePath, root.right)
+    } else {
+        1 + goodNodes_rec(root.`val`, root.left) + goodNodes_rec(root.`val`, root.right)
+    }
+}
