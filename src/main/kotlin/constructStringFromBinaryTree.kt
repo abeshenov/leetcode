0a00eced949dce14b7fa3711f8e6e389a1d18fb5
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-06 23:54:59 -0500

    Can be shorter

--- src/main/kotlin/constructStringFromBinaryTree.kt
+++ src/main/kotlin/constructStringFromBinaryTree.kt
@@ -2,10 +2,8 @@
 // https://leetcode.com/problems/construct-string-from-binary-tree/
 
 fun tree2str(root: TreeNode?): String =
-    if (root != null) tree2strHelper(root) else ""
-
-private fun tree2strHelper(root: TreeNode): String =
     when {
+        root == null -> ""
         root.left == null && root.right == null -> root.`val`.toString()
         root.right == null -> "${root.`val`}(${tree2str(root.left)})"
         else -> "${root.`val`}(${tree2str(root.left)})(${tree2str(root.right)})"
