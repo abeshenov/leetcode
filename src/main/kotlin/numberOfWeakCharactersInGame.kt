a373912373aba6fee45002fa7e17abf5b413f413
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-09 12:41:41 -0500

    In-place version

--- src/main/kotlin/numberOfWeakCharactersInGame.kt
+++ src/main/kotlin/numberOfWeakCharactersInGame.kt
@@ -1,6 +1,32 @@
 // 2022-09-09
 // https://leetcode.com/problems/the-number-of-weak-characters-in-the-game/
 
+// Shorter / quicker in-place version
+fun numberOfWeakCharactersInPlace(properties: Array<IntArray>): Int {
+    // Example of our custom sorting:
+    // [ (6,1), (6,9), (5,9), (5,9), (4,4), (3,6), (3,10), (2,1), (2,5), (1,10) ]
+    val arrayComparator: Comparator<IntArray> =
+        compareBy<IntArray> { -it[0] }
+            .then(compareBy { it[1] } )
+
+    properties.sortWith(arrayComparator)
+
+    var maxDefense = Int.MIN_VALUE
+    var result = 0
+
+    for (prop in properties) {
+        if (prop[1] < maxDefense) {
+            result++
+        } else {
+            maxDefense = prop[1]
+        }
+    }
+
+    return result
+}
+
+//------------------------------------------------------------------------------
+
 fun numberOfWeakCharacters(properties: Array<IntArray>): Int {
     val customSortedCharacters: List<Character> = properties.map { arrayToCharacter(it) }
         .sortedWith(characterComparator)
@@ -21,10 +47,6 @@ fun numberOfWeakCharacters(properties: Array<IntArray>): Int {
 
 private data class Character(val attack: Int, val defense: Int)
 
-/**
- * Example of our custom sorting:
- * [ (6,1), (6,9), (5,9), (5,9), (4,4), (3,6), (3,10), (2,1), (2,5), (1,10) ]
- */
 private val characterComparator: Comparator<Character> =
     compareBy<Character> { -it.attack }
         .then(compareBy { it.defense } )
