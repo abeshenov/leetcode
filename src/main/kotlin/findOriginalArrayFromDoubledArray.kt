00a2e78762990f994fc8c20a17a6fec00454c19d
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-14 21:05:56 -0500

    2022-09-15

---
+++ src/main/kotlin/findOriginalArrayFromDoubledArray.kt
@@ -0,0 +1,41 @@
+// 2022-09-15
+// https://leetcode.com/problems/find-original-array-from-doubled-array/
+
+fun findOriginalArray(changed: IntArray): IntArray {
+    if (changed.size % 2 != 0) {
+        return intArrayOf()
+    }
+
+    val frequencies: MutableMap<Int,Int> = mutableMapOf()
+
+    for (elem in changed) {
+        frequencies[elem] = frequencies.getOrDefault(elem, 0) + 1
+    }
+
+    val result = IntArray(changed.size/2)
+    var pos = 0
+
+    changed.sort()  // in-place
+
+    for (elem in changed) {
+        val frequency = frequencies[elem]!!
+
+        if (frequency <= 0) {
+            continue  // the element was deleted before
+        }
+
+        result[pos] = elem
+        pos++
+
+        frequencies[elem] = frequency - 1
+
+        val frequencyOfDouble = frequencies[2*elem]
+        if (frequencyOfDouble == null || frequencyOfDouble <= 0) {
+            return intArrayOf()
+        }
+
+        frequencies[2*elem] = frequencyOfDouble - 1
+    }
+
+    return result
+}
