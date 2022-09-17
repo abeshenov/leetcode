314a894e1799c3de66021d081aca9f8aadc03a79
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-17 16:34:28 -0500

    Prefix tree (Trie)

---
+++ src/main/kotlin/Trie.kt
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/
+
+class Trie {
+    private val root: TrieNode = TrieNode()
+
+    private data class TrieNode(
+        val children: MutableMap<Char,TrieNode> = mutableMapOf(),
+        var isWord: Boolean = false
+    )
+
+    fun insert(word: String) {
+        var nodePtr: TrieNode = root
+
+        for (chr in word) {
+            nodePtr.children.computeIfAbsent(chr) { TrieNode() }
+            nodePtr = nodePtr.children[chr]!!
+        }
+
+        nodePtr.isWord = true
+    }
+
+    fun search(word: String): Boolean {
+        var nodePtr: TrieNode = root
+
+        for (chr in word) {
+            if (nodePtr.children[chr] == null) {
+                return false
+            }
+            nodePtr = nodePtr.children[chr]!!
+        }
+
+        return nodePtr.isWord
+    }
+
+    fun startsWith(prefix: String): Boolean {
+        var nodePtr: TrieNode = root
+
+        for (chr in prefix) {
+            if (nodePtr.children[chr] == null) {
+                return false
+            }
+            nodePtr = nodePtr.children[chr]!!
+        }
+
+        return true
+    }
+}
