073e707b63c540ed3bf72d4089d3032f210112ca
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-18 14:54:59 -0500

    Don't use indices

--- src/main/kotlin/trappingRainWater.kt
+++ src/main/kotlin/trappingRainWater.kt
@@ -25,21 +25,16 @@ private fun List<Int>.max(): Int =
 // To optimize this solution, we need a better way to calculate the arrays
 //   leftMaximums  := height.slice(0..i).max()
 //   rightMaximums := height.slice(i..height.lastIndex).max()
-
-fun trap(height: IntArray): Int {
-    val leftMaximums = leftMaximums(height)
-    val rightMaximums = rightMaximums(height)
-
-    return height.indices.sumOf { i ->
-        Math.min(leftMaximums[i], rightMaximums[i]) - height[i]
-    }
-}
-
+//
 // This is "running reduce"!
-// runningMaximum[0] = height[0]
-// runningMaximum[1] = max(height[0], height[1])
-// runningMaximum[2] = max(height[0], height[1], height[2])
-// ...
+//   runningMaximum[0] = height[0]
+//   runningMaximum[1] = max(height[0], height[1])
+//   runningMaximum[2] = max(height[0], height[1], height[2])
+//   ...
+
+fun trap(height: IntArray): Int =
+    leftMaximums(height).zip(rightMaximums(height), Math::min).sum() -
+            height.sum()
 
 private fun leftMaximums(height: IntArray): List<Int> =
     height.runningReduce(Math::max)
