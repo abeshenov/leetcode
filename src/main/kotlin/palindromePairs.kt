e6f231612870e1b73d37fdd65455c9beb998d361
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-18 09:32:42 -0500

    2022-09-17 .. 2022-09-18

---
+++ src/main/kotlin/palindromePairs.kt
@@ -0,0 +1,80 @@
+// 2022-09-17
+// https://leetcode.com/problems/palindrome-pairs/
+
+fun palindromePairs(words: Array<String>): List<List<Int>> =
+    Helper(words).calculate()
+
+private class MyTrieNode {
+    var children: MutableMap<Char, MyTrieNode> = mutableMapOf()
+    var index: Int? = null
+    var list: MutableList<Int> = mutableListOf()
+}
+
+private class Helper(val words: Array<String>) {
+    val trie = MyTrieNode()
+    val result: MutableList<List<Int>> = mutableListOf()
+
+    init {
+        words.withIndex().forEach { addWord(it.index, it.value) }
+    }
+
+    private fun addWord(index: Int, word: String) {
+        var triePtr: MyTrieNode = trie
+        for (i in word.indices.reversed()) {
+            val chr = word[i]
+            triePtr.children.computeIfAbsent(chr) { MyTrieNode() }
+
+            if (word.isPalindromeUpTo(i)) {
+                triePtr.list.add(index)
+            }
+
+            triePtr = triePtr.children[chr]!!
+        }
+        triePtr.list.add(index)
+        triePtr.index = index
+    }
+
+    private fun search(index: Int) {
+        var triePtr: MyTrieNode = trie
+        for (j in words[index].indices) {
+            if (triePtr.index != null && triePtr.index != index && words[index].isPalindromeStartingFrom(j)) {
+                result.add(listOf(index, triePtr.index!!))
+            }
+            triePtr = triePtr.children[words[index][j]] ?: return
+        }
+        for (j in triePtr.list) {
+            if (index == j) continue
+            result.add(listOf(index, j))
+        }
+    }
+
+    fun calculate(): List<List<Int>> {
+        words.indices.forEach { search(it) }
+        return result
+    }
+
+    //--------------------------------------------------------------------------
+
+    // Checks if the word is palindrome in the interval (i..j)
+    private fun String.isPalindrome(start: Int, end: Int): Boolean {
+        var left = start
+        var right = end
+
+        while (left < right) {
+            if (get(left) != get(right)) {
+                return false
+            }
+            left++
+            right--
+        }
+
+        return true
+    }
+
+    private fun String.isPalindromeUpTo(end: Int): Boolean =
+        isPalindrome(0, end)
+
+    private fun String.isPalindromeStartingFrom(start: Int): Boolean =
+        isPalindrome(start, lastIndex)
+
+}
---
+++ src/main/kotlin/trappingRainWater.kt
@@ -0,0 +1,51 @@
+// 2022-09-18
+// https://leetcode.com/problems/trapping-rain-water/submissions/
+
+// Note that an element height[i] will trap
+// min(max(height[0..i]), max(height[i..])) - height[i]
+//
+//       ▓
+//   ▓░░░▓▓░▓
+// ▓░▓▓░▓▓▓▓▓▓
+
+private fun trapInefficient(height: IntArray): Int =
+    height.indices.sumOf { i ->
+        Math.min(
+            height.slice(0..i).max(),
+            height.slice(i..height.lastIndex).max()
+        ) - height[i]
+    }
+
+private fun List<Int>.max(): Int =
+    if (isEmpty()) 0 else this.reduce(Math::max)
+
+//------------------------------------------------------------------------------
+
+// To optimize this solution, we need a better way to calculate the arrays
+//   leftMaximums  := height.slice(0..i).max()
+//   rightMaximums := height.slice(i..height.lastIndex).max()
+
+fun trap(height: IntArray): Int {
+    if (height.isEmpty()) {
+        return 0
+    }
+
+    val leftMaximums = leftMaximums(height)
+    val rightMaximums = rightMaximums(height)
+
+    return height.indices.sumOf { i ->
+        Math.min(leftMaximums[i], rightMaximums[i]) - height[i]
+    }
+}
+
+// This is "running reduce"!
+// runningMaximum[0] = height[0]
+// runningMaximum[1] = max(height[0], height[1])
+// runningMaximum[2] = max(height[0], height[1], height[2])
+// ...
+
+private fun leftMaximums(height: IntArray): List<Int> =
+    height.runningReduce(Math::max)
+
+private fun rightMaximums(height: IntArray): List<Int> =
+    height.reversed().runningReduce(Math::max).reversed()
