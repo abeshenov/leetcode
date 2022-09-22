9b94c2f88afb32ccb5a8c00dbf7e4bd993b48f80
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-22 16:01:28 -0500

    Stack solution

--- src/main/kotlin/binaryTreeInorderTraversal.kt
+++ src/main/kotlin/binaryTreeInorderTraversal.kt
@@ -4,9 +4,30 @@
 // difficulty: easy
 //------------------------------------------------------------------------------
 
+import java.util.Stack
+
 fun inorderTraversal(root: TreeNode?): List<Int> =
     if (root == null) {
         listOf()
     } else {
         inorderTraversal(root.left) + listOf(root.`val`) + inorderTraversal(root.right)
     }
+
+fun inorderTraversalStack(root: TreeNode?): List<Int> {
+    val result: MutableList<Int> = mutableListOf()
+    val stack: Stack<TreeNode> = Stack()
+    var ptr: TreeNode? = root
+
+    while (ptr != null || stack.isNotEmpty()) {
+        if (ptr != null) {
+            stack.push(ptr)
+            ptr = ptr.left
+        } else {
+            ptr = stack.pop()
+            result.add(ptr.`val`)
+            ptr = ptr.right
+        }
+    }
+
+    return result
+}
