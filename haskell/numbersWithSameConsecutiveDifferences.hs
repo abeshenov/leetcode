f3f7c7e4bef7a5e7629fcd2461621cfd812d6606
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-09-29 00:59:55 -0500

    Helper --> '

--- haskell/numbersWithSameConsecutiveDifferences.hs
+++ haskell/numbersWithSameConsecutiveDifferences.hs
@@ -5,19 +5,19 @@
 --------------------------------------------------------------------------------
 
 numsSameConsecDiff :: Integer -> Integer -> [Integer]
-numsSameConsecDiff n k = concatMap (numsSameConsecDiffHelper n k) [1 .. 9]
+numsSameConsecDiff n k = concatMap (numsSameConsecDiff' n k) [1 .. 9]
 
-numsSameConsecDiffHelper :: Integer -> Integer -> Integer -> [Integer]
-numsSameConsecDiffHelper 1 k firstDigit =
+numsSameConsecDiff' :: Integer -> Integer -> Integer -> [Integer]
+numsSameConsecDiff' 1 k firstDigit =
   [firstDigit | 0 <= firstDigit && firstDigit <= 9]
-numsSameConsecDiffHelper numOfDigits k firstDigit =
+numsSameConsecDiff' numOfDigits k firstDigit =
   if 0 <= firstDigit && firstDigit <= 9
     then map (addFirstDigit (numOfDigits - 1) firstDigit) smallerNumbers
     else []
   where
     smallerNumbers =
-      numsSameConsecDiffHelper (numOfDigits - 1) k (firstDigit + k) ++
-      numsSameConsecDiffHelper (numOfDigits - 1) k (firstDigit - k)
+      numsSameConsecDiff' (numOfDigits - 1) k (firstDigit + k) ++
+      numsSameConsecDiff' (numOfDigits - 1) k (firstDigit - k)
     addFirstDigit numOfDigits digit num = num + 10^numOfDigits*digit
 
 
