6e97f753fc78746f5a45e283f9f229fc835d2dcd
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-11-17 23:53:10 -0600

    Formatting

--- src/main/kotlin/uglyNumber.kt
+++ src/main/kotlin/uglyNumber.kt
@@ -8,4 +8,4 @@ fun isUgly(n: Int): Boolean =
     listOf(2, 3, 5).fold(n) { m, p -> reduceFactors(m, p) } == 1
 
 private tailrec fun reduceFactors(m: Int, p: Int): Int =
-    if (m == 0 || m%p != 0) m else reduceFactors(m/p, p)
+    if (m == 0 || m % p != 0) m else reduceFactors(m / p, p)
